b10d66a23fce1c17d607db2409f15df36c0bb623
misiu29 <719547803@qq.com>
2021-01-19 22:59:43 +0800

    First

---
+++ Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.设置ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.关于ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 229);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(82, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(82, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(84, 311);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "开始游戏";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(82, 105);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "label3";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(171, 239);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.设置ToolStripMenuItem,
+            this.关于ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(246, 25);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 设置ToolStripMenuItem
+            // 
+            this.设置ToolStripMenuItem.Name = "设置ToolStripMenuItem";
+            this.设置ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.设置ToolStripMenuItem.Text = "设置";
+            this.设置ToolStripMenuItem.Click += new System.EventHandler(this.设置ToolStripMenuItem_Click);
+            // 
+            // 关于ToolStripMenuItem
+            // 
+            this.关于ToolStripMenuItem.Name = "关于ToolStripMenuItem";
+            this.关于ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.关于ToolStripMenuItem.Text = "关于";
+            this.关于ToolStripMenuItem.Click += new System.EventHandler(this.关于ToolStripMenuItem_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(84, 271);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "注册账号";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(55, 154);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(125, 12);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "您现在可以进行游戏了";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(19, 181);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(215, 12);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "如果你还没有账号的话 先请注册一个吧";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(19, 211);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(197, 12);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "*游戏内昵称请不要输入中文 会乱码";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(84, 291);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "退出";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(246, 358);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Launcher";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 设置ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 关于ToolStripMenuItem;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        private string strIP = "Your IP";
+        private string fileContent = "start Bin\\launcher.exe -masterip Your IP -lang schinese -enablecustom";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            //PingIp(strIP);
+            //label1.Visible = false;
+            //System.Threading.Thread.Sleep(10000);
+            label2.Visible = false;
+            label3.Visible = false;
+            label4.Visible = false;
+            label5.Visible = false;
+            label6.Visible = false;
+            button1.Visible = false;
+            button2.Visible = false;
+            button3.Visible = false;
+            button4.Visible = false;
+            button5.Visible = false;
+            label1.Text = "正在连接服务器，请稍等";
+            
+            PingIp(strIP);
+        }
+        /// <summary>
+          /// ping ip,测试能否ping通
+          /// </summary>
+          /// <param name="strIP">IP地址</param>
+          /// <returns></returns>
+          public void PingIp(string strIP)
+          {
+              //bool bRet = false;
+              try
+             {
+                 Ping pingSend = new Ping();
+                 PingReply reply = pingSend.Send(strIP, 1000);
+                if (reply.Status == IPStatus.Success)
+                {//bRet = true;
+                    System.Diagnostics.Debug.WriteLine("Success");
+                    MessageBox.Show("服务器连接成功", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    label4.Visible = true;
+                    label5.Visible = true;
+                    label6.Visible = true;
+                    label1.Text = "服务器连接正常";
+                    //label2.Text = reply.ToString();
+                    label2.Visible = true;
+                    label2.Text = "Ping:" + reply.RoundtripTime.ToString() + "ms";
+                    label3.Visible = true;
+                    button2.Visible = true;
+                    button4.Visible = true;
+                    db();
+                }
+                else 
+                {
+                    System.Diagnostics.Debug.WriteLine("Failed");
+                    MessageBox.Show("服务器连接失败", "坏起来了", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    label1.Text = "连接超时 服务器可能\n\n离线或者本机网络连接\n\n出现故障";
+                    button5.Visible = true;
+                }
+                    
+            }
+             catch (Exception e)
+             {
+                //bRet = false;
+                //MessageBox.Show("ip格式不正确\n连ip格式都能写错\nnt一定是个啥b", "坏起来了", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show(e.Message.ToString(), "坏起来了", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                System.Diagnostics.Debug.WriteLine("Failed");
+            }
+             //return bRet;
+         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "正在检测服务器连接";
+            PingIp(strIP);
+        }
+        public void writeBATFile(string fileContent)
+        {
+            string filePath = @"start.bat";
+            if (!File.Exists(filePath))
+            {
+                FileStream fs1 = new FileStream(filePath, FileMode.Create, FileAccess.Write);//创建写入文件
+                StreamWriter sw = new StreamWriter(fs1);
+                sw.WriteLine(fileContent);//开始写入值
+                sw.Close();
+                fs1.Close();
+                System.Diagnostics.Process.Start("start.bat");
+                Application.Exit();
+            }
+            else
+            {
+                /*
+                FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Write);
+                StreamWriter sr = new StreamWriter(fs);
+                sr.WriteLine(fileContent);//开始写入值
+                sr.Close();
+                fs.Close();*/
+                System.Diagnostics.Process.Start("start.bat");
+                Application.Exit();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            writeBATFile(fileContent);
+        }
+        /// <summary>
+        /// 数据库连接
+        /// </summary>
+        private const string conn = "mongodb://Your IP:27017";
+        /// <summary>
+        /// 指定数据库
+        /// </summary>
+        private const string dbName = "cso2";
+        /// <summary>
+        /// 指定集合(使用BsonDocument直接定义 不需要了)
+        /// </summary>
+        //private const string collectionName = "usersessions";
+        private void db()
+        {
+            var client = new MongoDB.Driver.MongoClient(conn);
+            IMongoDatabase db = client.GetDatabase(dbName);
+            var collection = db.GetCollection<BsonDocument>("usersessions");
+            //label3.Text = collection.ToString();
+            FilterDefinitionBuilder<BsonDocument> builderFilter = Builders<BsonDocument>.Filter;
+            //总条目数
+            var result = collection.Find<BsonDocument>(builderFilter.Empty).Count();
+            
+            Console.WriteLine(result);
+            if (result == 0)
+            {
+                label3.Text = "当前没有玩家在线";
+            }
+            else 
+            {
+                label3.Text = "当前有" + result.ToString() + "名玩家在线";
+            }
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            db();
+        }
+
+        private void 设置ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("因为nt是条懒狗 所以这里什么也没有", "坏起来了", MessageBoxButtons.OK);
+        }
+
+        private void 关于ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("by:misiu29\nVersion:1.0", "女子", MessageBoxButtons.OK);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2();
+            f2.Show();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
